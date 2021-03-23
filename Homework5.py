33dc2ee129e8103b0fa7f793361b2775a3dff182
bukiayegbusi <76229372+bukiayegbusi@users.noreply.github.com>
2021-03-23 14:48:05 +0000

    Add files via upload

---
+++ Homework5.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+dataset = pd.read_csv("/Users/bukiaiyegbusi/Desktop/Women In Data/Homework/Homework 5")
+dataset.shape
+
+
+
